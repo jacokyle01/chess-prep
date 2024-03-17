87679a818bd29968d2f464991fba1ce39c3ffc99
Kyle Jacobson <jacokyle01@gmail.com>
2024-03-16 21:11:13 -0500

    add component to advance to next trainable position

---
+++ src/Next.tsx
@@ -0,0 +1,9 @@
+interface Props {
+	learnNext: () => void;
+}
+
+export const Next = ({ learnNext }: Props) => {
+	return <button id="next" onClick={learnNext}></button>;
+};
+
+// export default Next;
