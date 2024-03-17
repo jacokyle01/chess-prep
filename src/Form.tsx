ea1ec7ce4a794df27fdbd25b9e6b0e9d842e6f25
Kyle Jacobson <jacokyle01@gmail.com>
2024-03-16 21:10:18 -0500

    add form for entering a PGN

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 interface FormProps {
-	initializeTraining: () => void;
+	initializeTraining: (input: string) => void;
 }
 
 export const Form: React.FC<FormProps> = ({ initializeTraining }) => {
+	const [userInput, setUserInput] = useState("");
+
+	const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+		setUserInput(e.currentTarget.value);
+		// alert(e.currentTarget.value);
+	};
+
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		initializeTraining();
+		initializeTraining(userInput);
+		setUserInput("Successfully entered PGN");
 	};
 
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type="text" />
+			<input
+				value={userInput}
+				onChange={handleChange}
+				type="text"
+				placeholder="Enter PGN..."
+			/>
 			<button>Submit</button>
 		</form>
 	);
